186f46e9368c5129507faf5d03c2e1f07a5947f0
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-19 22:21:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-import os,time
-from data import ui
-from data import cep
-from data import cpf
-from data import ip
-## Tabela de cores ANSI (Python) \\ OK
-
 global C,R,O,W,L,E,Y,a,l,e,i,s,t,ë,r,XV,XXI
+C='\033[1;30m';R='\033[1;31m';O='\033[1;32m';W='\033[1;33m';L='\033[1;34m';E='\033[1;35m';Y='\033[1;36m';XV='\033[1;37m';XXI='\033[0;0m';a='\033[1;40m';l='\033[1;41m';e='\033[1;42m';i='\033[1;43m';s='\033[1;44m';t='\033[1;45m';ë='\033[1;46m';p='\033[;7m'
 
-# fonte #
-C='\033[1;30m'     # Preto 
-R='\033[1;31m'     # Vermelho 
-O='\033[1;32m'     # Verde 
-W='\033[1;33m'    # Amarelo 
-L='\033[1;34m'     # Azul 
-E='\033[1;35m'     # Rosa
-Y='\033[1;36m'     # Ciano
+def restart():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+    
+import os,sys,time,json,subprocess,platform
+try:
+	from data import ui
+	from data import cep
+	from data import cpf
+	from data import ip
+except:
+	print(f"{C}[{R}*{C}] Arquivo Corrompido!");exit()
+	
+try:
+    if __name__ == '__main__':
+        ui.dialog('Buscando atualizações ...')
+        update = subprocess.check_output('git pull', shell=True)
+        if 'Already up to date' not in update.decode():
+            ui.dialog('Atualização instalada.\nReiniciando...')
+            restart()
+        else:
+            print(f'{C}[{Y}i{C}] Nenhuma atualizacao disponivel.')
+            time.sleep(2)
+except:
+    if os.path.exists('.git'):
+        pass
+    else:
+        ui.error_dialog('Falta de repositório GIT local')
 
-##
-XV='\033[1;37m'     # Branco
-XXI='\033[0;0m'      # Remover
-##
-
-# fundo #
-a='\033[1;40m'     # Preto
-l='\033[1;41m'      # Vermelho
-e='\033[1;42m'     # Verde
-i='\033[1;43m'      # Amarelo
-s='\033[1;44m'     # Azul
-t='\033[1;45m'      # Rosa
-ë='\033[1;46m'     # Ciano
-p='\033[;7m'          # Inverte 
-
-###
+try:
+    subprocess.check_output('apt update -y', shell=True)
+    os.system("apt install figlet curl -y")
+except:
+    os.system("pacman -Sy figlet curl")
 
 Sair=False
 while(Sair==False):
 	ui.banner()
 	ui.menu()
 	op = input(f'{p}{XV}Digite sua opção:{XXI} ').upper()
-	print(f'{XV}Aguarde...')
-	time.sleep(3)
 	if op == '1':
 		ip.consultar()
 	elif op == '2':
 		cpf.consultar()
 	elif op == '3':
 		cep.consultar()
-	
 	elif op == 'S':
 		os.system('clear')
 		Sair=True
@@ -56,4 +56,3 @@ while(Sair==False):
 		ui.grupozap()
 	else:
 		ui.error()
-		
